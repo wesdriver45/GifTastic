f4c66c8739b68145ec45bea08c81b8d95ad9e28d
wesdriver45 <wesdriver45@gmail.com>
2017-11-03 23:34:12 -0400

    lost functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,92 @@
 // api_key=dc6zaTOxFJmzC
 
 var sports = ["skydiving", "rock climbing", "mountain biking", "whitewater kayaking", "motorcross", "base jumping", "snowboarding", "paintball", "caving"];
+
       
-// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-//         sport + "&api_key=dc6zaTOxFJmzC&limit=10";
+ $(document).ready(function() {
 
-        $(document).ready(function() {
-        	renderButtons();
-        })
+    renderButtons();
 
 
-//ajax call
-    //   $.ajax({
-    //   url: queryURL,
-    //   method: 'GET'
-    // })
-    //   .done(function(response) {
-    //   console.log(response);
-    // });
+//on click for all buttons
+   $("button").on("click", function() {
+      var xSport = $(this).attr("data-name");
 
-//render button function
-	function renderButtons() {
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + xSport + "&api_key=dc6zaTOxFJmzC&limit=10";
+        
+        
+    //ajax call
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+              })
 
-        // Deleting the movie buttons prior to adding new movie buttons
-        // (this is necessary otherwise we will have repeat buttons)
+        .done(function(response) {
+        console.log(response);
+        
+        //store results
+        var results = response.data;
+        //narrow down rating to g
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            //making a div for the gifs
+            var gifDiv = $("<div class = 'item'>");
+            //store the rating
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            //making an img
+            var sportImage = $("<img>");
+
+            sportImage.attr("src", results[i].images.fixed_height_still.url);
+
+            //append ratin and gifDiv to div
+            gifDiv.append(p);
+            gifDiv.append(sportImage);
+
+            $("#gifs-appear-here").prepend(gifDiv);
+            }
+           }
+        });
+
+      //click to add movie to array from input
+      $("#add-sport").on("click", function(enent) {
+        event.preventDefault();
+
+        var sportButton = $("#sports-input").val().trim();
+
+        sports.push(sportButton);
+
+        renderButtons();
+      });
+    
+    //render button function
+  function renderButtons() {
+
+        //empty button div
         $("#buttons-here").empty();
 
-        // Looping through the array of movies
+        // loop through the array of movies
         for (var i = 0; i < sports.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array.
-          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
           var newSport = $("<button>");
-          // Adding a class
+          
           newSport.addClass("sport");
-          // Adding a data-attribute with a value of the movie at index i
+          
           newSport.attr("data-name", sports[i]);
-          // Providing the button's text with a value of the movie at index i
+          
           newSport.text(sports[i]);
-          // Adding the button to the HTML
+          
           $("#buttons-here").append(newSport);
-        }
-      }
+
+          $("#sports-input").val("");
+          }
+       }
+     });
+ });
 
 
-      // $("")
+
+
 
  //pause/animate gifs
    // $(".gif").on("click", function() {
@@ -60,3 +102,20 @@ var sports = ["skydiving", "rock climbing", "mountain biking", "whitewater kayak
    //      $(this).attr("src", $(this).attr("data-still"));
    //      $(this).attr("data-state", "still");
    //    }
+
+
+
+
+ 
+
+
+      
+
+
+
+  
+
+
+
+      
+
